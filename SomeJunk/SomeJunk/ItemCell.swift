5cabbda447e6e4a97a8195bf1bc38f897bdbd20c
Manju Bhandary <bhandary@yahoo.com>
2016-03-31 12:33:32 -0700

    Updated the Core Data project

---
+++ SomeJunk/SomeJunk/ItemCell.swift
@@ -0,0 +1,28 @@
+//
+//  ItemCell.swift
+//  SomeJunk
+//
+//  Created by manbhand on 3/30/16.
+//  Copyright © 2016 Manju Bhandary. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+}
--- SomeJunk/SomeJunk/MainVC.swift
+++ SomeJunk/SomeJunk/MainVC.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class MainVC: UIViewController {
+class MainVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
 
+    @IBOutlet weak var segment: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,16 @@ class MainVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
 
